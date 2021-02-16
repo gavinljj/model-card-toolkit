d3ac32926123db751b12393381fe9898be5f3b92
Karan Shukla <karanshukla@google.com>
2021-02-16 14:28:48 -0800

    add Bazel support
    PiperOrigin-RevId: 357805676

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Setup to install the Model Card Toolkit."""
+"""Setup to install the Model Card Toolkit.
 
+Run with `python3 setup.py sdist bdist_wheel`.
+"""
+
+from distutils import spawn
+from distutils.command import build
+
+import os
+import platform
+import subprocess
+
+from setuptools import Command
 from setuptools import find_packages
 from setuptools import setup
 
@@ -39,6 +50,72 @@ __version__ = globals_dict['__version__']
 with open('README.md', 'r', encoding='utf-8') as fh:
   _LONG_DESCRIPTION = fh.read()
 
+
+class _BuildCommand(build.build):
+  """Build everything that is needed to install.
+
+  This overrides the original distutils "build" command to to run bazel_build
+  command before any sub_commands.
+
+  build command is also invoked from bdist_wheel and install command, therefore
+  this implementation covers the following commands:
+    - pip install . (which invokes bdist_wheel)
+    - python setup.py install (which invokes install command)
+    - python setup.py bdist_wheel (which invokes bdist_wheel command)
+  """
+
+  def _build_cc_extensions(self):
+    return True
+
+  # Add "bazel_build" command as the first sub_command of "build". Each
+  # sub_command of "build" (e.g. "build_py", "build_ext", etc.) is executed
+  # sequentially when running a "build" command, if the second item in the tuple
+  # (predicate method) is evaluated to true.
+  sub_commands = [
+      ('bazel_build', _build_cc_extensions),
+  ] + build.build.sub_commands
+
+
+class _BazelBuildCommand(Command):
+  """Build Bazel artifacts and move generated files."""
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    self._bazel_cmd = spawn.find_executable('bazel')
+    if not self._bazel_cmd:
+      self._bazel_cmd = spawn.find_executable('bazelisk')
+    if not self._bazel_cmd:
+      raise RuntimeError(
+          'Could not find "bazel" or "bazelisk" binary. Please visit '
+          'https://docs.bazel.build/versions/master/install.html for '
+          'installation instruction.')
+    self._additional_build_options = []
+    if platform.system() == 'Darwin':  # see b/175182911 for context
+      self._additional_build_options = ['--macos_minimum_os=10.9']
+
+  def run(self):
+    # Bazel should be invoked in the directory containing the WORKSPACE file.
+    mct_dir = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), 'model_card_toolkit')
+    proto_dir = os.path.join(mct_dir, 'proto')
+    subprocess.check_call(
+        [
+            self._bazel_cmd, 'build', '--verbose_failures',
+            *self._additional_build_options, ':model_card_py_pb2'
+        ],
+        cwd=proto_dir,
+    )
+    subprocess.check_call(
+        [
+            self._bazel_cmd, 'run', *self._additional_build_options,
+            ':move_generated_files'
+        ],
+        cwd=mct_dir,
+    )
+
+
 setup(
     name='model-card-toolkit',
     version=__version__,
@@ -48,11 +125,9 @@ setup(
     url='https://github.com/tensorflow/model-card-toolkit',
     author='Google LLC',
     author_email='tensorflow-extended-dev@googlegroups.com',
-    packages=find_packages(),
+    packages=find_packages(exclude=('bazel-model_card_toolkit*',)),
     package_data={
-        'model_card_toolkit': [
-            'schema/**/*.json', 'template/**/*.jinja'
-        ]
+        'model_card_toolkit': ['schema/**/*.json', 'template/**/*.jinja']
     },
     python_requires='>=3.6,<4',
     install_requires=REQUIRED_PACKAGES,
@@ -77,4 +152,7 @@ setup(
     ],
     license='Apache 2.0',
     keywords='model card toolkit ml metadata machine learning',
-)
+    cmdclass={
+        'build': _BuildCommand,
+        'bazel_build': _BazelBuildCommand,
+    })
